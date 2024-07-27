e36f4d8dad155de5032e23f77707aa16ae983145
Shrinidhi-KJ <86567235+Shrinidhi-KJ@users.noreply.github.com>
2024-07-28 03:50:20 +0530

    Update info.py

--- info.py
+++ info.py
@@ -3,8 +3,8 @@
 # Define the cookies as a list of dictionaries
 cookies = [
     {
-        'name': '.AspNet.ApplicationCookie',
-        'value': 'CB1Y-8QyrWmBlMRoD-lT4kQAr57dPrAYNKzrLB_OZLI21LzGWir9l327UDoZjlRYJsV_xWPdc9WTggdth24RTSPGxZ-kQ5TorwSMBo8YF-0CwEdKlfv3YD2cnmI4PblpP_ezj-2lMmq80PWMW1jvK3-vTh7GtHHoEzga5GmlPlB7AQheELltNpxK1oBrVKZHI9FzRaBof_bmDhZ1SMnEdVnwyLm4MmNLsBqKWPrCqOUSNfEpYjJpvo4YUrtnEr1VmlD0ApHhS4FonJKO9qEpELE9915MafEXAhPx_XR_eeEQsDoPLQckgsZrzVyOg8-Pn4FfJ5cXsnwo6d3D9tTvkzBHU30U94RV9pekspkb3oAmWWsTgt91xU4xSqZFKGqpGmjscvEWwOW3IxCC5rYEwmIc3tm5PGA4xyZ2wHyHjlvHDyW1pNoLoHTmmenfj-e-plqemIBuM7D0hsxWVfvRn1sGfKFa0pISZ1ERvRl2W3Y9sh_OpRKQb9Rz_yQKstTSWofmcqO2Usf5zlEszzxEc8iWuhtYpoW3Q2Rdk-VW9jpGOWR8HmvRUGXfHGprGexmeVEuEygYF_jmSdtjFFps3LuwmM1lOLnEIaf_DKaSuXBqIiqWry64qfW3GpnBg__A1AxYDxEkC4Mfn0_JCdHJlhkHzdezUOdAjD_oqiW-7lk2_jOAMxeZiC30OXGX0ZagpUysLOdOVqv-IKF7NO56zGtAcvsbF6qXtIpyZyL_OndMzeheotYY3rCatJdkMWtkf32FZH32DoRrbuUtWtF4M05jZ6xLxJy5Y15AqAZQBiLN8GQzG3zfa1xtdaxcTrKSuOn-k_RfqVeaS4UJjE6QBdPLBeuPlRqTpvXjISVxIfZSXp22sUi73dOnO1FMT17RN4zigQWqadt3et7EOKOncdyA4vvjg6Cqze2iPcBOlysECKFMZSzhjqp5kxAoGsn0gvJd_4kGGyXOSxg8aQs09y8i9rVyHugO3sxlbXF',
+        'name': 'Session ID/name',
+        'value': '*** Value to be added ***',
         'domain': 'www.usvisascheduling.com',
         'path': '/',
         'secure': True,
